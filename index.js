479bf4fc52307ad16a91c6c13efc3f3ca874b59e
Marko1212 <shihonage7@gmail.com>
2021-04-15 22:04:27 +0200

    make it better

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
 
 //const CONNECTION_URL = "mongodb+srv://Marko1212:123123123@cluster0.ycss7.mongodb.net/test?retryWrites=true&w=majority";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => app.listen(PORT, () => console.log(`Server running on port : ${PORT}`)))
