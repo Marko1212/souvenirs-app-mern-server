17899c4d942bc4172f6ab457b12e03986189bbe4
Marko1212 <shihonage7@gmail.com>
2021-05-07 10:31:18 +0200

    update

--- routes/posts.js
+++ routes/posts.js
@@ -1,16 +1,17 @@
 import express from "express";
 
 import {getPosts, createPost, updatePost, deletePost, likePost, getNumberOfPosts } from '../controllers/posts.js';
+import auth from '../middleware/auth.js';
 
 const router = express.Router();
 
 // http://localhost:5000/posts
 
 router.get('/', getPosts);
-router.post('/', createPost);
-router.patch('/:id', updatePost);
-router.delete('/:id', deletePost);
-router.patch('/:id/likePost', likePost);
+router.post('/', auth, createPost);
+router.patch('/:id', auth, updatePost);
+router.delete('/:id', auth, deletePost);
+router.patch('/:id/likePost', auth, likePost);
 
 router.get("/countPosts", getNumberOfPosts);
 
